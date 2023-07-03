d4249de168a78cb621e5db91e42d23439aeeb66f
Igor Bandero <igorbandero@gmail.com>
2023-07-03 13:58:57 -0300

    Requisito: POST /participants

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from "dotenv"
 import Joi from "joi"
+import dayjs from 'dayjs';
+
 const PORT = 5000;
 
 const app = express();
@@ -16,6 +18,54 @@ let db
 mongoClient.connect()
     .then(() => db = mongoClient.db())
     .catch((err) => console.log(err.message))
+
+app.post('/participants', async (req, res) => {
+
+    const { name } = req.body;
+    
+    const schemaParticipant = Joi.object({
+        name: Joi.string().required().min(1)
+    })
+    
+    const validation = schemaParticipant.validate(req.body, {abortEarly: false})
+    
+    if (validation.error){
+        return res.sendStatus(422);
+    }
+
+    const user = await db.collection("participants").findOne({ name: name });
+
+    if (user) {
+        return res.sendStatus(409);
+    } 
+
+    const newParticipant = {
+		name: name,
+		lastStatus: Date.now()
+    }
+
+    const dataHoje = dayjs();
+    const horaAtual = dataHoje.format('HH:mm:ss'); 
+
+    console.log(newParticipant.lastStatus);
+
+    const logMessage = { 
+		from: name,
+		to: 'Todos',
+		text: 'entra na sala...',
+		type: 'status',
+		time: horaAtual
+    }
+    
+    try {
+        await db.collection("participants").insertOne(newParticipant)
+        await db.collection("messages").insertOne(logMessage)
+        res.sendStatus(201)
+    }
+    catch (err){
+        res.status(500).send(err.message)
+    }
+});
 
 app.listen(PORT, console.log(`Servidor rodando na porta ${PORT}`));
 
